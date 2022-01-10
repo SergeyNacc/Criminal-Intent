b0e85b383531f6af53d08276d6574999d236fb8b
SergeyNacc <sergeynaksin@gmail.com>
2022-01-10 18:40:41 +0300

    added Converter for table

--- app/src/main/java/nacc/sergey/criminal_intent/database/CrimeDatabase.kt
+++ app/src/main/java/nacc/sergey/criminal_intent/database/CrimeDatabase.kt
@@ -2,8 +2,10 @@ package nacc.sergey.criminal_intent.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import nacc.sergey.criminal_intent.Crime
 
 @Database(entities = [Crime::class], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
 }
