289a977380b03f7e2318a38bdad20f4d3f8404e9
SergeyNacc <sergeynaksin@gmail.com>
2022-01-12 18:59:00 +0300

    added files for Database

--- app/src/main/java/nacc/sergey/criminal_intent/CrimeListViewModel.kt
+++ app/src/main/java/nacc/sergey/criminal_intent/CrimeListViewModel.kt
@@ -6,14 +6,7 @@ import androidx.lifecycle.ViewModel
 //Генерирование преступлений
 class CrimeListViewModel : ViewModel() {
 
-    val crimes = mutableListOf<Crime>()
-
-    init {
-        for (i in 0 until 100) {
-            val crime = Crime()
-            crime.title = "Crime #$i"
-            crime.isSolved = i % 2 == 0
-            crimes += crime
-        }
-    }
+    //доступ к репозиторию
+    private val crimeRepository = CrimeRepository.get()
+    val crimes = crimeRepository.getCrimes()
 }
